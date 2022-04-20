f57bca82c968a1f297aee778f3c1ca86a5e2a605
David Whitaker <damwhit@gmail.com>
2022-04-20 10:08:41 -0400

    Add base url to api docs and remove pages dir to avoid confusion

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <API apiDescriptionUrl="https://raw.githubusercontent.com/govly/govly-api-docs/main/docs.yaml" />
+      <API 
+        basePath="govly-api-docs"
+        apiDescriptionUrl="https://raw.githubusercontent.com/govly/govly-api-docs/main/docs.yaml" 
+      />
     </div>
   );
 }
